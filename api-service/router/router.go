02dd8681bc60afc96d13cdc86b0c67898e2b8bf0
Paul Chang <a2323269@gmail.com>
2020-11-17 01:54:49 +0800

    feat: implement router to handle the APIs using Gin

---
+++ api-service/router/router.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	"api-service/api"
+)
+
+// Initialize the gin router
+func Initialize() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	productAPI := r.Group("/api/product")
+	{
+		productAPI.GET("/getAll", api.GetAllProducts)
+	}
+
+	cartAPI := r.Group("/api/cart")
+	{
+		cartAPI.GET("/getAllByAccountID", api.GetAllCartsByAccountID)
+		cartAPI.POST("/addCart", api.AddCart)
+		cartAPI.POST("/editCart", api.EditCart)
+	}
+
+	orderAPI := r.Group("/api/order")
+	{
+		orderAPI.GET("/getAllByAccountID", api.GetAllOrdersByAccountID)
+		orderAPI.GET("/getAllItemsByOrderID", api.GetAllOrderItemsByOrderID)
+	}
+
+	return r
+}
