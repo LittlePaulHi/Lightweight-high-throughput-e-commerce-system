1d78d1fb53a9b9f8f81390e412a1a206802b4584
LittlePaulHi <a2323269@gmail.com>
2020-11-28 14:56:31 +0000

    feat: implement kafka producer for the purchase event API

--- api-service/go.mod
+++ api-service/go.mod
@@ -3,6 +3,7 @@ module api-service
 go 1.15
 
 require (
+	github.com/Shopify/sarama v1.27.2
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
@@ -11,8 +12,6 @@ require (
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/swaggo/gin-swagger v1.2.0
@@ -22,11 +21,11 @@ require (
 	github/littlepaulhi/highly-concurrent-e-commerce-lightweight-system/pkg v0.0.0-00010101000000-000000000000
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	golang.org/x/sys v0.0.0-20201022201747-fb209a7c41cd // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/tools v0.0.0-20201114224030-61ea331ec02b // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/gorm v1.20.6
 )
 
---
+++ api-service/internal/kafka/sync/producer.go
@@ -0,0 +1,84 @@
+package sync
+
+import (
+	"encoding/json"
+	config "github/littlepaulhi/highly-concurrent-e-commerce-lightweight-system/pkg/configs"
+	"github/littlepaulhi/highly-concurrent-e-commerce-lightweight-system/pkg/database/mariadb"
+	"github/littlepaulhi/highly-concurrent-e-commerce-lightweight-system/pkg/kafka/model"
+	"log"
+	"os"
+
+	"github.com/Shopify/sarama"
+	"github.com/spf13/viper"
+)
+
+type Kafka struct {
+	Producer sarama.SyncProducer
+}
+
+func init() {
+	mariadb.Setup()
+	viper.AutomaticEnv()
+	viper.SetConfigName("config-kafka")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$PROJECT_PATH/pkg/configs/")
+}
+
+func CrateNewSyncProducer() sarama.SyncProducer {
+	var configuration config.Configuration
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error when reading kafka config file, %s", err)
+	}
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		log.Fatalf("Unable to decode the kafka config into struct, %v", err)
+	}
+
+	// setup log to stdout for debug
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
+
+	sconfig := sarama.NewConfig()
+	sconfig.Producer.RequiredAcks = sarama.WaitForAll // Wait for all in-sync replicas to ack the message
+	sconfig.Producer.Retry.Max = 5
+	sconfig.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(configuration.Kafka.BrokerList, sconfig)
+	if err != nil {
+		log.Fatal("Failed to start Sarama SyncProducer:", err)
+	}
+
+	return producer
+}
+
+func (kafka *Kafka) Publish(topic string, cartIDs []int) error {
+	purchaseMsg := model.PurchaseMessage{CartIDs: cartIDs}
+	purchaseMsgBytes, err := json.Marshal(purchaseMsg)
+	if err != nil {
+		log.Fatalf("Convert purchase message struct to bytes occurs error: %v\n", err)
+		return err
+	}
+
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(purchaseMsgBytes),
+	}
+
+	partition, offset, err := kafka.Producer.SendMessage(msg)
+	if err != nil {
+		log.Fatalf("Failed to store your data:, %v\n", err)
+		return err
+	}
+	log.Printf("Purchase message is stored with unique identifier important partition: %v, offset: %v\n", partition, offset)
+
+	return nil
+}
+
+func (kafka *Kafka) Close() error {
+	if err := kafka.Producer.Close(); err != nil {
+		log.Fatal("Failed to close the kafka producer", err)
+	}
+
+	return nil
+}
