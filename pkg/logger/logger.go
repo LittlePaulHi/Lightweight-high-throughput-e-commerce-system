2ea5776bf34459ec91858f26607927f09f0664cf
LittlePaulHi <a2323269@gmail.com>
2020-12-10 14:43:38 +0000

    feat: added logger for redis client

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,10 +9,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var log *logrus.Logger
-var APILog *logrus.Entry
-var ServiceLog *logrus.Entry
-var InitLog *logrus.Entry
+var (
+	log        *logrus.Logger
+	InitLog    *logrus.Entry
+	APILog     *logrus.Entry
+	ServiceLog *logrus.Entry
+	RedisLog   *logrus.Entry
+)
 
 func init() {
 	log = logrus.New()
@@ -26,10 +29,10 @@ func init() {
 		FieldsOrder:     []string{"component", "category"},
 	}
 
+	InitLog = log.WithFields(logrus.Fields{"API_Service": "Init"})
 	APILog = log.WithFields(logrus.Fields{"API_Service": "API"})
 	ServiceLog = log.WithFields(logrus.Fields{"API_Service": "Service"})
-	InitLog = log.WithFields(logrus.Fields{"API_Service": "Init"})
-
+	RedisLog = log.WithFields(logrus.Fields{"Redis": "Cache"})
 }
 
 func SetLogLevel(level logrus.Level) {
