480ebe24a79c0ac9f9e1ec64db4b6456304a9aaf
Paul Chang <a2323269@gmail.com>
2020-12-12 01:57:31 +0800

    perf: added connection pool settings for the MariaDB

--- pkg/configs/mariadb.go
+++ pkg/configs/mariadb.go
@@ -1,8 +1,15 @@
 package config
 
+import (
+	"time"
+)
+
 type MariadbConfiguration struct {
-	Type string
-	User string
-	Host string
-	Name string
+	Type            string
+	User            string
+	Host            string
+	Name            string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
 }
