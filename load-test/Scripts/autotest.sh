91272df3690a8ecb49ed66db350f9d4fb95de2a5
Gary Xiao <garyh0205@gmail.com>
2020-12-19 23:10:20 +0800

    Separate restart procedure

--- load-test/Scripts/autotest.sh
+++ load-test/Scripts/autotest.sh
@@ -3,32 +3,47 @@
 stages=( "getallproducts_test" "getAllOrderByAccountID_test" "getAllOrderItemsByOrderID_test" "addCart_test" "getAllCartsByAccountID_test" "editCart_test" "PurchaseFromCarts_test" "integrated_test" )
 tag="async"
 
+
 for directory in "${stages[@]}"
 do
-    for ((vus=500; vus<=3000;vus+=500))
+    for ((vus=10; vus<=10;vus+=500))
     do
-        echo "Restart apiserver"
-        ssh apiserver "TAG=${tag} sh ./autorestart.sh"
+        echo "Stop kafka"
+        ssh ppf204@kafka "cd ~/kafka-docker/;docker-compose stop;docker-compose rm -vfs"
         sleep 0.5
 
-        echo "Restart kafka"
-        ssh ppf204@kafka "cd ~/kafka-docker/;docker-compose down;docker-compose rm -vfs;docker-compose -f ./docker-compose-single-broker.yml up -d"
-        sleep 0.5
+        echo "Stop redis"
+        ssh redis "cd ~/redis/;docker-compose stop;docker-compose rm -vfs"
 
-        echo "Restart redis"
-        ssh redis "cd ~/redis/;docker-compose down;docker-compose rm -vfs;docker-compose up -d"
-        sleep 0.5
+        echo "Stop consumer"
+        ssh consumer "TAG=${tag} sh ./stop.sh"
 
-        echo "Restart consumer"
-        ssh consumer "TAG=${tag} sh ./autorestart.sh"
-        sleep 0.5
-
+        echo "Stop apiserver"
+        ssh apiserver "TAG=${tag} sh ./stop.sh"
+        
         echo "Restore Database"
         mysql --defaults-extra-file=config < ppfinal.sql
+
+        echo "Start kafka"
+        ssh ppf204@kafka "cd ~/kafka-docker/;docker-compose -f ./docker-compose-single-broker.yml up -d --no-recreate"
         sleep 0.5
 
-        k6 run -e TIMES=${vus} --out influxdb=http://192.168.0.5:8086/${directory} "../${directory}.js"
-        sleep 0.5
+        sleep 30s
+
+        echo "Start redis"
+        ssh redis "cd ~/redis/;docker-compose up -d"
+
+        echo "Start consumer"
+        ssh consumer "TAG=${tag} sh ./start.sh"
+
+        echo "Start apiserver"
+        ssh apiserver "TAG=${tag} sh ./start.sh"
+        
+        sleep 1m
+
+        ./k6 run -e TIMES=${vus} --out influxdb=http://192.168.0.5:8086/${directory} "../${directory}.js"
     done
 done
 
+
+#        sleep 0.5
